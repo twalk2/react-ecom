2efb230ce5a3430d275e574177a6ac795f651f7b
twalk2 <2twalk2@gmail.com>
2020-02-13 10:22:34 -0700

    added more styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 
+import Header from "./headernavbar/header";
+import Navbar from "./headernavbar/navbar";
+
 export default class Layout extends Component {
   render() {
-    return <div className="layout">{this.props.children}</div>;
+    return (
+      <div className="layout">
+        {this.props.children}
+        <Header />
+        <Navbar />
+      </div>
+    );
   }
 }
