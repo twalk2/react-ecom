7d6952659a478ffdccaf7a8b90ab57d3e7147f33
twalk2 <2twalk2@gmail.com>
2020-02-13 09:48:02 -0700

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>ecom</h1>
+        {/* {...this.props.children} */}
+      </div>
+    );
+  }
+}
