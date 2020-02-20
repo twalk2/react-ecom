8e1db24af875c6814dde9cfcdf218d1f5368ae7a
twalk2 <2twalk2@gmail.com>
2020-02-20 13:48:41 -0800

    added reducer as form and setup devtools extension

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 
+import { reduxForm, Field } from "redux-form";
+
 export default class SignIn extends Component {
   render() {
     return <div className="sign-in">sign in</div>;
   }
 }
+
+SignIn = reduxForm({
+  form: "SignIn"
+})(SignIn);
