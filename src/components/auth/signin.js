ae46a671daf123b96035715a51c7546814c8df23
twalk2 <2twalk2@gmail.com>
2020-03-13 15:34:40 -0600

    built form button componenet in formFields.js

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import SignInForm from "./signinForm";
 
 export default class SignIn extends Component {
+  onSubmit = fields => {
+    console.log(fields);
+  };
+
   render() {
     return (
       <div className="sign-in">
-        <SignInForm className="sign-in__form" />
+        <SignInForm onSubmit={this.onSubmit} className="sign-in__form" />
       </div>
     );
   }
