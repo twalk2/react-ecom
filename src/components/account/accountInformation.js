0d3f677dbf1a7d345bfdcce711e4588b286faabc
twalk2 <2twalk2@gmail.com>
2020-04-27 10:40:33 -0600

    built account information form

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 
+import PageTitle from "../pageTitle";
+import AccountInformationForm from "./accountInformationForm";
+
 class AccountInformation extends Component {
+  onSubmit = fields => {
+    console.log(fields);
+  };
+
   render() {
-    return <div>Account Information</div>;
+    return (
+      <div className="account-information">
+        <PageTitle
+          className="account-information"
+          title="Account Information"
+        />
+        <AccountInformationForm
+          onSubmit={this.onSubmit}
+          className="account-information__form"
+        />
+      </div>
+    );
   }
 }
 
